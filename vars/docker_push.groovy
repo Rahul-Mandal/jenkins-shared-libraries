1f12924d7bba487858172760a55ef0e7deed2b4a
Rahul-Mandal <47028462+Rahul-Mandal@users.noreply.github.com>
2024-10-25 10:44:01 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,10 +1,17 @@
-def call(String Project, String ImageTag, String dockerhubuser){
-   withCredentials([usernamePassword('credentialsId':"dockerhubcred",passwordVariable:"dockerHubPass",usernameVariable:"dockerHubUser")]){
+// def call(String Project, String ImageTag, String dockerhubuser){
+//    withCredentials([usernamePassword(credentialsId:"dockerhubcred",passwordVariable:"dockerHubPass",usernameVariable:"dockerHubUser")]){
                        
                   
-                sh 'docker login -u ${dockerHubUser} -p ${dockerHubPass}'
-   }
-                sh 'docker image tag ${Project}:${ImageTag} ${dockerHubUser}/${Project}:${ImageTag}'
-                sh 'docker push ${dockerhubuser}/${Project}:${ImageTag}'
+//                 sh 'docker login -u ${dockerHubUser} -p ${dockerHubPass}'
+//    }
+//                 // sh 'docker image tag ${Project}:${ImageTag} ${dockerHubUser}/${Project}:${ImageTag}'
+//                 sh 'docker push ${dockerhubuser}/${Project}:${ImageTag}'
    
+// }
+
+def call(String Project, String ImageTag, String dockerhubuser){
+  withCredentials([usernamePassword(credentialsId: 'dockerhubcred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
+      sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
+  }
+  sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
 }
